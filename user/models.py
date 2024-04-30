0a4e3eb628d8a73984ea161ca27ba36731eb5d6c
Ravi Maurya <lcs2020043@iiitl.ac.in>
2024-04-30 11:50:24 +0530

    added user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    pass
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    # profile_pic = models.ImageField(
+    #     null=True,
+    #     blank=True,
+    # )
+
+    objects = UserManager()
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
